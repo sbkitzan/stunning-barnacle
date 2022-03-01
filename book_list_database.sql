20913cd61306a1478c378bb045dde9584945b96a
sbkitzan <100728982+sbkitzan@users.noreply.github.com>
2022-03-01 14:25:22 -0700

    Create book_list_database.sql

---
+++ book_list_database.sql
@@ -0,0 +1,5 @@
+CREATE TABLE booklist (id INTEGER PRIMARY KEY, name TEXT, rating Integer);
+INSERT INTO booklist Values (1, "A Court of Thrones and Roses", 5);
+INSERT INTO booklist Values (2, "From Blood and Ash", 5);
+INSERT INTO booklist values (3, "The Wretched of Muirwood", 5);
+SELECT * FROM booklist;
